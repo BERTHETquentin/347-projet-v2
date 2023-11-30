c00e3f690c5ac042ec4f4774e73f27513b4145e3
BERTHETquentin <quentin@berthet.ch>
2023-11-30 15:41:02 +0100

    frontend

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,10 +1,14 @@
 // import css
 import "../styles/App.css";
+// import header
+import Header from "./Header";
+// import mui
+import '@fontsource/roboto/300.css';
 
 function App() {
   return (
     <>
-      <p>salut</p>
+      <Header />
     </>
   );
 }
