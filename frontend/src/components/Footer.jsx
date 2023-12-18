b272b20fd9d40cb457f45c90474b7a377618ec41
BERTHETquentin <quentin@berthet.ch>
2023-12-18 14:23:09 +0100

    doc of footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,14 +1,22 @@
+import React from "react";
+import "./Footer.css";
+
+/**
+ * Footer component.
+ * @returns {JSX.Element} The rendered Footer component.
+ */
 const Footer = () => {
   return (
-    <footer class="footer">
-      <div class="content has-text-centered">
+    <footer className="footer">
+      <div className="content has-text-centered">
         <p>
           <strong>347 Projet v2</strong> by{" "}
           <a href="https://github.com/BERTHETquentin">Quentin Berthet</a> & <a href="https://github.com/nicolasloi">Nicolas Loison</a>. The source code is not
-          licensed.
+          licensed. 
         </p>
       </div>
     </footer>
   );
 };
+
 export default Footer;
